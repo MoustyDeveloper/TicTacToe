fe200414de234fe98ce1cb43013a1b82bda75285
moustycoder <mossnader@gmail.com>
2018-04-27 15:42:22 +1000

    up to friday arvo

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var whichPlayer = 1
+    var whichPlayer = 1  //variable to store each player or computer turn
+    
+    var whichState = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    let winState = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15], [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [0, 5, 10, 15], [3, 6, 9, 12]]
+    
+    var IsActive = true
     
     @IBAction func placeImage(_ sender: AnyObject) {
+        
+        if (whichState[sender.tag-1] == 0 && IsActive == true)
+        {
+           whichState[sender.tag-1] = whichPlayer
         
         if (whichPlayer == 1)
         {
@@ -23,6 +33,8 @@ class ViewController: UIViewController {
         {
             sender.setImage(UIImage(named: "Nought.png"), for: UIControlState())
             whichPlayer = 1
+        }
+            
         }
     }
     
